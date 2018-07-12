2b888eb1fc3fc0fb6c117da8bc595934a0dab5be
Samantha Berk <sberk10@gmail.com>
2018-07-11 21:48:36 -0700

    printing answer choices

--- src/components/Quiz/AnswerChoices.js
+++ src/components/Quiz/AnswerChoices.js
@@ -1,11 +1,18 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
-const answerChoices = (props) => (
-  <div className ="col s8 quiz-answer-choices">
-    <h3>{props.answers}</h3>
-  </div>
-);
+const answerChoices = (props) => {
+  const answers = props.answers.map((choice) => {
+    return (
+      <p>{choice}</p>
+    )
+  });
+  return (
+    <div className ="col s8 quiz-answer-choices">
+      {answers}
+    </div>
+  );
+}
 
 answerChoices.propTypes = {
   answers: PropTypes.array.isRequired
