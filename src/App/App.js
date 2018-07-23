ff5757d60ec691b77c70d387219fc805b38e0219
Samantha Berk <sberk10@gmail.com>
2018-07-22 23:54:24 -0700

    fix error page

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Home from '../components/Home/Home';
 import Quiz from '../components/Quiz/Quiz';
 import Itinerary from '../components/Itinerary/Itinerary';
 import NotFound from '../components/NotFound/NotFound';
+import LoginPage from '../components/auth/LoginPage';
 
 import './App.css';
 
@@ -27,7 +28,7 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="wrapper">
+          <Switch>
           <Route path="/" exact component={Home}/>
           <Route path="/quiz"
           render={ (props) => <Quiz {...props} changeState={ this.updateState } />}
@@ -35,8 +36,9 @@ class App extends Component {
           <Route path="/itinerary" exact
           render={ (props) => <Itinerary {...props} userAnswers={ this.state.userAnswers } />}
           />
+          <Route path="/login" component={LoginPage}/>
           <Route component={NotFound} />
-        </div>
+          </Switch>
       </BrowserRouter>
     );
   }
